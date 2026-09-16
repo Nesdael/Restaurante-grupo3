6084e2a2557f9c3b6c301b3f1a3d7abc60c7aa06
Nesdael <danieldurfue7@gmail.com>
2026-09-15 21:55:02 -0500

    docs: set real swagger metadata and rewrite the readme

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,20 +1,25 @@
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import { INestApplication, Logger } from "@nestjs/common";
+import { INestApplication, Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const logger = new Logger('Swagger');
 
 export const swaggerConfiguration = (app: INestApplication): void => {
+  const config = new DocumentBuilder()
+    .setTitle('Restaurant API')
+    .setDescription(
+      'Restaurant management platform: tables, menu and reservations.',
+    )
+    .setVersion('1.0')
+    .addTag('Health', 'Service status')
+    .addTag('Tables', 'Physical tables of the restaurant')
+    .addTag('Categories', 'Menu categories')
+    .addTag('Products', 'Menu products')
+    .addTag('Menu', 'Public menu')
+    .addTag('Reservations', 'Table reservations')
+    .build();
 
-    const config = new DocumentBuilder()
-        .setTitle('Add Description')
-        .setDescription('Loading')
-        .setVersion('Loading')
-        .addBearerAuth()
-        .addApiKey({ type: 'apiKey', name: 'x-api-key', in: 'header' }, 'API Key')
-        .build()
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api/docs', app, document)
-
-    logger.log(`Docs available at http://localhost:3000/api/docs`)
-}
+  logger.log('Docs available at /api/docs');
+};
